382683fe343c0104398924fedbbe17e2ea55884c
samanthavictoria <sasavicsouza@gmail.com>
2020-05-19 00:58:00 +0900

    function

--- www/components/calcular.js
+++ www/components/calcular.js
@@ -1,2 +1,13 @@
-$(document).on("click","#virg")
+function botao(num){
+  var salvo= document.calc.visor.value;
+  document.calc.visor.value = salvo + num;
+  //nao esta pegando oq tem dentro do form, nao achei um erro
+}
 
+function reset(){
+//limpar a tela
+}
+
+function calcule(){
+//pegar o valor do visor usando eval depois jogar nele dnv
+}
